cd4ed5c1b40a8bd0be089bda3144827909f9fb55
Aayush Bhat <aayushbhat2@gmail.com>
2023-07-12 16:13:40 -0700

    Implement get user

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from sqlite import return_table
+from sqlite import return_table, get_patient
 app = Flask(__name__)
 
 
@@ -13,10 +13,17 @@ def index():
 def show_all_patients():
     return render_template("all.html", patients=return_table())
 
-@app.route('/add', methods=['POST'])
-def about():
-    return render_template("all.html", patients=return_table())
+@app.route('/get-patient-prompt')
+def get_patient_prompt():
+    return render_template("get_patient_prompt.html")
 
+@app.route('/get-patient-result', methods=['GET', 'POST'])
+def get_patient_result():
+    if request.method == 'POST':
+      name = request.form['Name']
+      age = request.form['Age']
+      gender = request.form['Gender']
+      return render_template("all.html", patients=get_patient(name, age, gender))
 
 if __name__ == '__main__':
     app.run(debug=True)
